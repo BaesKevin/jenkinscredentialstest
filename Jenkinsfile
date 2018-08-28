577dcaf52d8361424bfc1fcf309d44913bf9ccfd
Kevin Baes <kevin.baes@student.howest.be>
2018-08-28 15:33:59 +0200

    first try to get credentials path in node

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,13 @@
 pipeline{
     agent { docker {image 'node:6.3'}}
+
+    tools {nodejs "node"}
+
     stages {
+        environment {
+            MY_SECRET = credentials('dev_gcal_creds')
+        }   
+        
         stage('run'){
             steps {
                 sh 'node app.js'
--- app.js
+++ app.js
@@ -3,43 +3,5 @@
 const http = require('http');
 const fs = require('fs');
 
-const requestHandler = (request, response) => {
-    console.log(request.url);
-    console.log(process.env);
-	// if( request.url === "/favicon.ico" ){
-	// 	serveFavicon(request, response);
-	// } else {
-	// 	response.setHeader("Content-Type","text/html");
-	// 	if (isPalindrome(request.url)) {
-	// 		response.end('<span style="border:1px solid red">Welcome to the PALINDOME page</span>');
-	// 	} else {
-	// 		response.end('<span style="border:1px solid red">Welcome to the standard page</span>');
-	// 	}
-		
-	// }
-
-};
-
-// function isPalindrome(str){
-// 	for(var i=0,j=str.length-1 ; i<j ; i++,j--){
-// 		console.log(str[i],"<->",str[j]);
-// 		if(str[i] !== str[j]) return false;
-// 	}
-// 	return true;
-// }
-
-// function serveFavicon(request, response){
-// 	fs.readFile('static/howestIcon.ico', (e,d)=>{
-// 		if (e) {
-// 			console.log("something went wrong while reading the image");
-// 		} else {
-// 			response.end(d);
-// 		}
-// 	});
-// }
-
-const server = http.createServer(requestHandler);
-
-server.listen(9000,()=>{
-	console.log("listening");
-});
+console.log(process.env);
+console.log(process.env.dev_gcal_creds);
