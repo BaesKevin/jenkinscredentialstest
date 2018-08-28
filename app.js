c34daa571194bac7466d16fa4b9656d2d0fb52ad
Kevin Baes <kevin.baes@student.howest.be>
2018-08-28 16:29:16 +0200

    try to log path

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ const fs = require('fs');
 
 const file = fs.readFileSync(process.env.MY_SECRET_PATH);
 
+const path = process.env.MY_SECRET_PATH.split('').join(' ');
+console.log(path);
+
 console.log('file contents: ' + file);
