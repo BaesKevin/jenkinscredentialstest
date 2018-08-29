485698bce8439537750fc1b9db09d5426bfd0308
Kevin Baes <kevin.baes@student.howest.be>
2018-08-29 10:57:40 +0200

    try mounting a directory in the working directory and mount it on the docker container

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,8 +13,12 @@ node {
                 string(credentialsId: 'GCAL_PRIVATE_KEY', variable: 'GCAL_PRIVATE_KEY')
             ]
         ) {
+            sh "mkdir -p ${WORKSPACE}/testvolume"
+            sh "cat 'content' > ${WORKSPACE}/testvolume/secretile.json"
             sh "docker build --no-cache -f production_dockerfile -t secretfiletest ."
-            sh "docker run --rm -e GCAL_PRIVATE_KEY=$GCAL_PRIVATE_KEY -e GCAL_PRIVATE_KEY_ID=$GCAL_PRIVATE_KEY_ID --name secretfiletest secretfiletest"
+            sh "docker run --rm -e GCAL_PRIVATE_KEY=$GCAL_PRIVATE_KEY -e GCAL_PRIVATE_KEY_ID=$GCAL_PRIVATE_KEY_ID -v ${WORKSPACE}/testvolume:/mountedtestvolume --name secretfiletest secretfiletest"
+            sh "rm /testvolume/secretile.json"
+            sh "rmdir /testvolume"
         }
     }
 }
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const fs = require('fs');
+const http = require('http');
 // const { exec } = require('child_process');
 // exec('ls -lR /secrets', (err, stdout, stderr) => {
 //   if (err) {
@@ -13,33 +14,49 @@ const fs = require('fs');
 //   console.log(`stderr: ${stderr}`);
 // });
 
-function createCalendarConfig() {
-    const template = {
-        type: "service_account",
-        project_id: "steadfast-bebop-176513",
-        private_key_id: "",
-        private_key: "",
-        client_email: "kevinbaes@steadfast-bebop-176513.iam.gserviceaccount.com",
-        client_id: "110761424735295424611",
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://accounts.google.com/o/oauth2/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/kevinbaes%40steadfast-bebop-176513.iam.gserviceaccount.com"
-    };
+// function createCalendarConfig() {
+//     const template = {
+//         type: "service_account",
+//         project_id: "steadfast-bebop-176513",
+//         private_key_id: "",
+//         private_key: "",
+//         client_email: "kevinbaes@steadfast-bebop-176513.iam.gserviceaccount.com",
+//         client_id: "110761424735295424611",
+//         auth_uri: "https://accounts.google.com/o/oauth2/auth",
+//         token_uri: "https://accounts.google.com/o/oauth2/token",
+//         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+//         client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/kevinbaes%40steadfast-bebop-176513.iam.gserviceaccount.com"
+//     };
 
-    template.private_key_id = process.env.GCAL_PRIVATE_KEY_ID;
-    template.private_key = process.env.GCAL_PRIVATE_KEY;
+//     template.private_key_id = process.env.GCAL_PRIVATE_KEY_ID;
+//     template.private_key = process.env.GCAL_PRIVATE_KEY;
     
-    return template;
-}
+//     return template;
+// }
 
-const config = createCalendarConfig();
+// const config = createCalendarConfig();
 
-fs.writeFile('service_account_credentials.json', JSON.stringify(config), 'utf8', (err) => {
-    if (err) console.log(err);
-}); // write it back 
+// fs.writeFile('service_account_credentials.json', JSON.stringify(config), 'utf8', (err) => {
+//     if (err) console.log(err);
+// }); // write it back 
 
-fs.readFile('service_account_credentials.json', 'utf8', (err, data) => {
+// fs.readFile('service_account_credentials.json', 'utf8', (err, data) => {
+//     if (err) console.log(err);
+//     console.log(data);
+// })
+
+const requestHandler = (request, response) => {
+    response.end("Hello world");
+
+};
+
+const server = http.createServer(requestHandler);
+
+fs.readFile('./mountedtestvolume/secretfile.json', 'utf8', (err, data) => {
     if (err) console.log(err);
     console.log(data);
-})
+});
+
+server.listen(8080,()=>{
+	console.log("listening");
+});
