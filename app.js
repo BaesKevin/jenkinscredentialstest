7434a21ca6c487feb384a9dac515975216be575b
Kevin Baes <kevin.baes@student.howest.be>
2018-08-29 11:38:00 +0200

    log ls error

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { exec } = require('child_process');
 exec('ls -lR /testvolume', (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
+    console.log(err);
     return;
   }
 
