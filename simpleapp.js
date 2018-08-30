bf091a9ee4c8bd8fcf6233d7a6d5b3b5d1680def
Kevin Baes <kevin.baes@student.howest.be>
2018-08-30 08:41:36 +0200

    syntax

--- simpleapp.js
+++ simpleapp.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 fs.readDirSync('/containersecrets').forEach(f => console.log('mounted file: '));
 fs.readFile('/containersecrets/secretfile.json', 'utf8', (err, data) => {
     if (err) console.log(err);
-    else console.log('secretfile data ':data);
+    else console.log('secretfile data ', data);
 });
 
